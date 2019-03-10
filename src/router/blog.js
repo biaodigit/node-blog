861f1f7d56143429e231e3ad01112bbc437e876a
luowendi <15622285015@163.com>
2019-03-10 17:28:23 +0800

    init router

---
+++ src/router/blog.js
@@ -0,0 +1,36 @@
+const handleBlogRouter = (req, res) => {
+    const method = req.method;
+
+    // 获取博客列表
+    if (method === 'GET' && req.path === '/api/blog/list') {
+        return {
+            msg: '这是获取博客列表接口'
+        }
+    }
+    // 获取博客详情
+    if (method === 'GET' && req.path === '/api/blog/detail') {
+        return {
+            msg: '这是获取博客详情接口'
+        }
+    }
+    // 新建一篇博客
+    if (method === 'POST' && req.path === '/api/blog/new') {
+        return {
+            msg: '这是新建博客接口'
+        }
+    }
+    // 更新一篇博客
+    if (method === 'POST' && req.path === '/api/blog/update') {
+        return {
+            msg: '这是更新博客接口'
+        }
+    }
+    // 删除一篇博客
+    if (method === 'POST' && req.path === '/api/blog/del') {
+        return {
+            msg: '这是删除博客接口'
+        }
+    }
+};
+
+module.exports = handleBlogRouter;
